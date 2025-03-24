12a5022a2a627552ee74d9d401ea5a76510a0240
salman221b <epsalmaan@gmail.com>
2025-03-24 17:05:55 +0530

    payment cahnges

--- backend/routes/book-appointment.js
+++ backend/routes/book-appointment.js
@@ -3,5 +3,25 @@ const router = express.Router();
 
 router.post("/book-appointment", (req, res) => {
   // Implement your appointment booking logic here
+  const {
+    userId,
+    doctor,
+    specialization,
+    selectedDate,
+    selectedSlot,
+    mode,
+    payment_mode,
+    payment_status,
+  } = req.body;
+  console.log(
+    userId,
+    doctor,
+    specialization,
+    selectedDate,
+    selectedSlot,
+    mode,
+    payment_mode,
+    payment_status
+  );
   res.json({ message: "Appointment booked successfully" });
 });
