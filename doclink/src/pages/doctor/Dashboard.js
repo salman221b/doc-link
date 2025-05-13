b54205a3730aa498b7af086ab71c4641fee10f68
salman221b <epsalmaan@gmail.com>
2025-05-13 22:11:15 +0530

    changes in register socket

--- doclink/src/pages/doctor/Dashboard.js
+++ doclink/src/pages/doctor/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../components/doctorNavbar/NavBar";
 import HeroSection from "./heroSection/HeroSection";
 import Carousel from "./heroSection/Carousel";
@@ -6,12 +6,19 @@ import TipsSection from "./tipsSection.js/TipsSection";
 import ScrollToTop from "../../components/scrollToTop/ScrollToTop";
 import DoctorIncomingCallPopup from "../../components/doctorPopup/DoctorPopup";
 import { jwtDecode } from "jwt-decode";
+import socket from "../../components/socket/socket";
 
 const Dashboard = () => {
   const token = localStorage.getItem("token");
   const decoded = token ? jwtDecode(token) : null;
 
   const doctorId = decoded?.id; // Extract user ID
+  console.log(doctorId);
+  useEffect(() => {
+    if (doctorId) {
+      socket.emit("register", { userId: doctorId });
+    }
+  }, [doctorId]);
   return (
     <div>
       <NavBar />
--- doclink/src/pages/user/Dashboard.js
+++ doclink/src/pages/user/Dashboard.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Dashboard.css";
 import NavBar from "../../components/userNavbar/NavBar";
 import Carousel from "./heroSection/Carousel";
 import HeroSection from "./heroSection/HeroSection";
 import TipsSection from "./tipsSection.js/TipsSection";
 import ScrollToTop from "../../components/scrollToTop/ScrollToTop";
+import socket from "../../components/socket/socket";
+import { jwtDecode } from "jwt-decode";
+
 const Dashboard = () => {
+  const token = localStorage.getItem("token");
+  const decoded = token ? jwtDecode(token) : null;
+
+  const patientId = decoded?.id;
+  useEffect(() => {
+    if (patientId) {
+      socket.emit("register", { userId: patientId });
+    }
+  }, [patientId]);
   return (
     <div>
       <NavBar />
