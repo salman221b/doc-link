e4e669c81234dd8ae28ef39bb15b28a7f9feace2
salman221b <epsalmaan@gmail.com>
2025-05-14 15:03:40 +0530

    changes in call room

--- doclink/src/components/callRoom/CallRoom.js
+++ doclink/src/components/callRoom/CallRoom.js
@@ -1,30 +1,34 @@
-import { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
 import { LiveKitRoom, VideoConference } from "@livekit/components-react";
-import "@livekit/components-styles";
+import "@livekit/components-styles"; // import default LiveKit UI styles
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const CallRoom = () => {
   const { roomName } = useParams();
-  const { identity } = useLocation().state;
-  const [token, setToken] = useState("");
+  const { identity, role, userName } = useLocation().state;
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     const getToken = async () => {
-      const res = await fetch(
-        "https://doc-link-backend.onrender.com/get-livekit-token",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ identity, roomName }),
-        }
-      );
+      try {
+        const res = await fetch(
+          "https://doc-link-backend.onrender.com/get-livekit-token",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ identity, roomName }),
+          }
+        );
 
-      const data = await res.json();
-      setToken(data.token);
+        const data = await res.json();
+        setToken(data.token);
+      } catch (error) {
+        console.error("Failed to fetch token:", error);
+      }
     };
 
     getToken();
-  }, [roomName, identity]);
+  }, [identity, roomName]);
 
   if (!token) return <div>Connecting...</div>;
 
@@ -32,8 +36,8 @@ const CallRoom = () => {
     <LiveKitRoom
       token={token}
       serverUrl="wss://doclink-gzihezdq.livekit.cloud"
-      data-lk-theme="default"
       connect={true}
+      data-lk-theme="default"
     >
       <VideoConference />
     </LiveKitRoom>
